bb1abc7bb6648df6084ea148ebb4e95d37e489af
Zelin Chen <6692czl@gmail.com>
2023-03-06 14:48:30 -0800

    2

--- q2.py
+++ q2.py
@@ -2,8 +2,17 @@
 # ******************************
 # Make your Code
 # ******************************
-
-
+L = []
+S = []
+while True:
+    n = input()
+    if n == 'STOP':
+        break
+    if len(n) > len(L) or L == []:
+        L = n
+    elif len(n) < len(S) or S == []:
+        S = n
+print(L, S)
 # Requirement
 # No need to use list
 # All input values are taken one by one separatively.
